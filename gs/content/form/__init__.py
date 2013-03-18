e09320b005d48f2d99e45f859c17ad3e9c32e5c1
Michael JasonSmith <mpj17@onlinegroups.net>
2013-03-18 12:11:23 +1100

    Adding the checkbox widget.

--- gs/content/form/__init__.py
+++ gs/content/form/__init__.py
@@ -1,5 +1,8 @@
 # coding=utf-8
+#lint:disable
+from checkbox import multi_check_box_widget
 from form import SiteForm
 from radio import radio_widget
 from select import select_widget
 from disabledtextwidget import disabled_text_widget
+#lint:enable
---
+++ gs/content/form/checkbox.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from zope.app.form.browser import MultiCheckBoxWidget
+from zope.app.form.browser.widget import renderElement
+
+
+class NotBrokenMultiCheckBoxWidget(MultiCheckBoxWidget):
+    _joinButtonToMessageTemplate = \
+      u'<span class="checkboxGroup" id="checkboxgroup-%s">%s&nbsp;%s</span>'
+
+    def renderItem(self, index, text, value, name, cssClass):
+        widgetId = '%s.%s' % (name, index)
+        elem = renderElement('input',
+                             type="checkbox",
+                             cssClass=cssClass,
+                             name=name,
+                             id=widgetId,
+                             value=value)
+        label = '<label class="checkboxLabel" for="%s">%s</label>' % \
+          (widgetId, text.decode("utf-8"))
+        gId = widgetId.replace('.', '-')
+
+        return self._joinButtonToMessageTemplate % (gId, elem, label)
+
+    def renderSelectedItem(self, index, text, value, name, cssClass):
+        widgetId = '%s.%s' % (name, index)
+        elem = renderElement('input',
+                             type="checkbox",
+                             cssClass=cssClass,
+                             name=name,
+                             id=widgetId,
+                             value=value,
+                             checked="checked")
+        label = '<label class="checkboxLabel" for="%s">%s</label>' % \
+          (widgetId, text)
+        gId = widgetId.replace('.', '-')
+        return self._joinButtonToMessageTemplate % (gId, elem, label)
+
+
+def multi_check_box_widget(field, request):
+    return NotBrokenMultiCheckBoxWidget(field,
+                                        field.value_type.vocabulary,
+                                        request)
