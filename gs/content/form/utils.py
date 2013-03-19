cb278023e39d40fbbe811a17ad3d684897ffac09
Michael JasonSmith <mpj17@onlinegroups.net>
2013-03-19 16:36:56 +1100

    Cleaning up the enforce_schema function

--- gs/content/form/utils.py
+++ gs/content/form/utils.py
@@ -1,9 +1,23 @@
 #coding: utf-8
 from zope.schema import *
-from zope.interface import implements, providedBy, implementedBy, \
-  directlyProvidedBy, alsoProvides
+from zope.interface import alsoProvides
 import logging
 log = logging.getLogger('gs.content.form.utils')
+
+
+TYPE_MAP = {
+    Text: 'text',
+    TextLine: 'string',
+    ASCII: 'text',
+    ASCIILine: 'string',
+    URI: 'string',
+    Bool: 'boolean',
+    Float: 'float',
+    Int: 'int',
+    Datetime: 'date',
+    Date: 'date',
+}
+
 
 def enforce_schema(inputData, schema):
     """
@@ -12,25 +26,12 @@ def enforce_schema(inputData, schema):
       * "inputData" will provide all the properties defined in "schema"
     """
 
-    typeMap = {
-      Text:      'text',
-      TextLine:  'string',
-      ASCII:     'text',
-      ASCIILine: 'string',
-      URI:       'string',
-      Bool:      'boolean',
-      Float:     'float',
-      Int:       'int',
-      Datetime:  'date',
-      Date:      'date',
-    }
     fields = [field[0] for field in getFieldsInOrder(schema)]
     for field in fields:
         if not hasattr(inputData, field):
             m = u'%s has no attr %s' % (inputData.getId(), field)
             log.info(m)
             default = schema.get(field).default or ''
-            t = typeMap.get(type(schema.get(field)), 'ustring')
+            t = TYPE_MAP.get(type(schema.get(field)), 'ustring')
             inputData.manage_addProperty(field, default, t)
     alsoProvides(inputData, schema)
-
